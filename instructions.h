fbc00f102f9a8170ec109e856cedc7b1b858f9d6
UnexomWid <mihaitinu@gmail.com>
2018-11-21 20:15:04 +0200

    Added execution time measurement

--- instructions.h
+++ instructions.h
@@ -1,8 +1,28 @@
+/*
+ * EXCS Interpreter - A cross-platform interpreter for Exom CrypticScript written in C++.
+ * Copyright (C) 2018  UnexomWid
+
+ * instructions.h - Contains the declarations of the instruction list and all instructions.
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include "instruction.h"
-#include <vector>
 
+#include <vector>
 
 extern std::vector<instruction> instruction_list;
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
-#include "instructions.h"
+/*
+ * EXCS Interpreter - A cross-platform interpreter for Exom CrypticScript written in C++.
+ * Copyright (C) 2018  UnexomWid
 
+ * main.cpp - Contains the entry point of the application.
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "instructions.h"
+#include "timer.h"
+
+#include <iostream>
 #include <cstdio>
 #include <fstream>
 #include <string>
@@ -8,6 +30,8 @@
 void error(const char *text);
 bool fileExists(const char* file);
 void interpret(std::ifstream &script, int argc, char *argv[]);
+
+CHRONOMETER chronometer;
 
 int main(int argc, char *argv[])
 {
@@ -38,6 +62,8 @@ void interpret(std::ifstream &script, int argc, char *argv[])
 {
 	try
 	{
+		chronometer = time_now();
+
 		int index = 0;
 		std::vector<char> pointer;
 		char current_char;
@@ -62,6 +88,7 @@ void interpret(std::ifstream &script, int argc, char *argv[])
 				printf("%c", current_char); throw std::runtime_error("Invalid instruction");
 			}
 		}
+		std::cout << "\nExecution took " << getf_exec_time_ns(chronometer);
 	}
 	catch (std::exception &e)
 	{

8206b640576ab3cb5bddaa78966547672c503e3d
UnexomWid <mihaitinu@gmail.com>
2019-09-22 09:01:58 +0300

    Refactored 'instructions' to 'instruction_handler'.

--- src/instruction_handler.cpp
+++ src/instruction_handler.cpp
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "instructions.h"
+#include "instruction_handler.h"
 #include <string>
 
 std::unordered_map<char, instruction> instruction_list;
